31034fa03585170a3adaf9b1699f29222ff3ef78
Olivier Radisson <olivier.radisson@insa-lyon.fr>
2013-04-11 19:34:20 +0200

    Corrige la boucle de compilation en déplaçant le dossier tex dans la racine du projet

--- TeXos.py
+++ TeXos.py
@@ -7,9 +7,11 @@ Launch the main windows of TeXos
 """
 
 from gi.repository import Gtk
+# Add the "src" directory to the python path 
 from sys import path as PYTHONPATH
 from os import getcwd, path
 PYTHONPATH.append(path.join(getcwd(),"src"))
+# --
 from src import gui
 
 if __name__ == '__main__':
